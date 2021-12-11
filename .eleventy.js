bae110cb4b8371c5e3b93b5b06b7c7edb45b7268
wommy <tom@technomad.media>
2021-12-10 20:45:21 -0500

    feat: public/

--- .eleventy.js
+++ .eleventy.js
@@ -3,15 +3,14 @@ module.exports = function (eleventyConfig) {
 	// Add filters to Nunjucks
 	eleventyConfig.addFilter("dateDisplay", require("./src/_filters/dates.js") );
 	
-	eleventyConfig.addPassthroughCopy("./src/css/")
-	eleventyConfig.addPassthroughCopy("./src/images/")
-	eleventyConfig.addWatchTarget("./src/css/")
-	eleventyConfig.addWatchTarget("./src/images/")
+	eleventyConfig.addPassthroughCopy({ "./public/css/": "css" })
+	eleventyConfig.addPassthroughCopy({ "./public/images/": "images" })
+	eleventyConfig.addWatchTarget("./public/")
 
 	return {
 		dir: {
 			input: "src",
-			output: "public"
+			output: "dist"
 		},
 		pathPrefix: "/eleventy-bragdoc/"
 	}
--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # bragdoc
 
 theme fork of [Emily Y Leung's](https://github.com/emilyyleung) [eleventy-bragdoc](https://github.com/emilyyleung/eleventy-bragdoc)
+
+## a few edits in order
+
+- [x] switch to yarn (personal preference)
+- [x] upgrade 11ty to @beta
+
+- [x] mv css & images from src to public && .gitignore
+- [x] switching output from public to dist
+
